5e65528d552a2bd1de7768dd6ff29a48967be783
karljohann <karljohann@hotmail.com>
2022-12-04 19:20:27 +0000

    data.py now works to load data to pandas dataframe

--- data.py
+++ data.py
@@ -7,42 +7,38 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-# TODO: Move STORAGE_PATH, output_file, and get_files to utils.py file
+# TODO: Move STORAGE_PATH to utils.py file
 
 DATA_FILES_PATH = "/Users/karljohann/Downloads/the_generator/csv/"
 STORAGE_PATH = "/Users/karljohann/Downloads/the_generator/"
-
-def get_files(filetype, dir = None):
-    files = []
-    for file_path in list(glob.glob(dir)):
-        _, ext = os.path.splitext(file_path)
-        if ext == '.mid':
-            files.append(file_path)
-        if os.path.isdir(file_path):
-            files += get_files(filetype, file_path + f"/*.{filetype}")
-
-    return files
-
-def output_file(filename, filetype, appendix=""):
-    return f"{STORAGE_PATH}{Path(filename).stem}{appendix}.{filetype}"
-
 
 
 def process_data(data):
     df = pd.concat(data, axis=0, ignore_index=True)
     train_set, test_set = train_test_split(df, test_size=0.1, random_state=123)
 
+    # train_set.info()
+    train_set.summary()
+
     y = train_set['note_int'].copy()
-    X = train_set.drop('note_int')
+    X = train_set.drop(columns=[
+        'note_int',
+        'time',
+        'end',
+        'velocity',
+    ])
+    # X = train_set.drop('note_int', axis=1)
+
+    return X, y
 
 
 
 if __name__ == "__main__":
-    files = get_files("csv", DATA_FILES_PATH + "/**") # all files and subfolders
+    files = glob.glob(os.path.join(DATA_FILES_PATH, "*.csv"))
     data = []
 
     for f in files:
         df = pd.read_csv(f, index_col=0)
         data.append(df)
-    
+
     process_data(data)
