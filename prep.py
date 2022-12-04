9a27c2c4ccb544f67a81ff13676444d274b57c0a
karljohann <karljohann@hotmail.com>
2022-12-04 12:53:18 +0000

    Rewrite of prep file

--- prep.py
+++ prep.py
@@ -1,54 +1,19 @@
 import csv
 import glob
 import os
-import midiutil
-import mido
-
-from collections import defaultdict
-from pprint import pprint
-
 from pathlib import Path
 
 import numpy as np
-import librosa
-import librosa. display
-import pyaudio
 
-import pretty_midi as pmidi
-
-import torch
+import pretty_midi as pm
 
 
-STORAGE_PATH = "/Users/karljohann/Downloads/the_generator/pkl/"
+INPUT_FILES_PATH = "/Users/karljohann/Downloads/archive/Jazz Midi"
+STORAGE_PATH = "/Users/karljohann/Downloads/the_generator/csv/"
 
-def minmax_velocity(amps, lower = 70, upper = 127):
-    ''' normalizes amplitude of onset to midi velocity '''
-    amps = np.array(amps)
-    amin = amps.min()
-    amax = amps.max()
-    return (amps - amin) / (amax - amin) * (upper - lower) + lower
 
 def output_file(filename, filetype, appendix=""):
-    # return STORAGE_PATH + Path(filename).with_suffix(filetype).name # FIXME: Is this needed?
-    return STORAGE_PATH + Path(filename).stem + appendix + "." + filetype
-    # return f"{STORAGE_PATH}{filename}.{filetype}"
-
-def write_midi(hits, bpm, file_name_no_extension):
-    MyMIDI = midiutil.MIDIFile(4)
-    MyMIDI.addTempo(0, 0, bpm)
-    quarter_note = 60 / bpm
-
-    for track in range(0, 4): # four hands, four tracks
-        MyMIDI.addProgramChange(track, 1, 0, 115) # 116 Woodblock
-
-    for note, velocity, onset_time, hand in zip(*hits):
-        # print(0, 0, int(note), onset_time / quarter_note, 0, (velocity))
-        # track = np.random.randint(2) # TODO: heuristic player guessage
-        track = np.random.randint(0, 4)
-        MyMIDI.addNote(track, 0, int(note), (onset_time / quarter_note), 0.01, int(velocity))
-
-    with open(output_file(file_name_no_extension, 'mid'), "wb") as f:
-        MyMIDI.writeFile(f)
+    return f"{STORAGE_PATH}{Path(filename).stem}{appendix}.{filetype}"
 
 
 def read_csv(filename):
@@ -59,96 +24,68 @@ def read_csv(filename):
     return data
 
 def write_csv(rows, filename, appendix=""):
-    out = ""
-    out += "timedelta,note,note_int,velocity,length\n"
-    # for timedelta, note, velocity, length in zip(row['timedelta'], row['note'], row['velocity'], row['length']):
+    out = f"{','.join(rows[0].keys())}\n"
     for row in rows:
-        out += f"{row['time']},{row['note']},{row['note_int']},{row['velocity']},{row['note_length']}\n"
-        # out += f"{timedelta},{note},{velocity},{length}\n"
+        out += f"{str(','.join([str(i) for i in row.values()]))}\n"
 
     with open(output_file(filename, 'csv', appendix), 'wt') as f:
         f.write(out)
 
-
 def processFile(f):
-    failed_files = []
-    try:
-        mid = mido.MidiFile(f)
-    # except OSError as e:
-    # except ValueError as e:
-    except Exception as e:
-        print(f"File ({Path(f).name}) failed:", e)
-        failed_files.append(Path(f).name)
-        return None
-    print("Failed files count:", len(failed_files), "\n", failed_files)
+    mid = pm.PrettyMIDI(f)
+
+    # time (ms) since last note (notes in chords all have same start time)
+    def getTimedelta(note, last_timedelta, last_note):
+        timedelta = (note.start - last_note.start)
+        # if timedelta == 0: # if first note(s) not at start of song (timedelta is initialised as 0)
+        #     return 0.0 # FIXME: dont have to, it should just be 0 (last_note is initialised as the first)
+
+        # if last_note.start == note.start: # same start time as last: chord (or first note)
+        #     return last_timedelta
+
+        if last_note.start == note.start: # same start time as last: chord (or first note)
+        # if last_note.start == note.start or last_note.end == note.end: # same start time as last: chord (or first note)
+            return last_timedelta
+
+        # if timedelta < 0: # TODO: Can probably combine this if with the one above
+        #     if last_note.end == note.end: # well, then you've got a chord, I guess
+        #         return last_timedelta
+        #     else:
+        #         return 0.0 # FIXME: not sure what's going on
+
+        return np.clip(timedelta, 0.0, 999)
+
 
     tracks = []
-    columns = {
-        'note': [],
-        'timedelta': [],
-        'velocity': [],
-        'length': [],
-    }
+    for i, instrument in enumerate(mid.instruments): # TODO: remove enumeration (only used for filename)
+        if not instrument.is_drum: # skip instruments that are not melodic
+            last_note = instrument.notes[0]
+            timedelta = 0
+            notes = []
 
-    def closeRow(note, timestamp, velocity, note_length):
-        return {
-            'note': librosa.midi_to_note(note),
-            'note_int': note,
-            'time': timestamp,
-            'velocity': velocity,
-            'note_length': note_length,
-        }
+            instr_notes = instrument.notes
+            instr_notes = sorted(instr_notes, key=lambda k: k.start)
+            for note in instr_notes:
+                timedelta = getTimedelta(note, timedelta, last_note)
+                last_note = note
 
-    for i, track in enumerate(mid.tracks):
-        last_seen_time = 0
-        elapsed_time = 0
-        notes = []
-        wait_for_off = defaultdict()
-        absolute_time = defaultdict()
+                notes.append({
+                    'note': pm.note_number_to_name(note.pitch),
+                    'time': round(note.start, 3),
+                    'end': round(note.end, 3),
+                    'velocity': note.velocity,
+                    'timedelta': round(timedelta, 3),
+                    'note_length': note.get_duration(),
 
-        n = 0
-        for msg in track:
-            elapsed_time += msg.time
+                    'note_int': note.pitch,
+                    'tick': mid.time_to_tick(note.start),
+                    'tickdelta': mid.time_to_tick(note.start - timedelta),
+                    'note_length_tick': mid.time_to_tick(note.get_duration()),
+                })
 
-            if msg.type == 'note_on' and msg.velocity > 0:
-                # TODO: Assume for overwriting notes and other notes without note_off
-                if msg.note in wait_for_off: # If note already in dict and has not been released
-                    row = closeRow(msg.note, msg.time, msg.velocity, 7)
-                    notes.append(row)
-                    del wait_for_off[msg.note]
-                    del absolute_time[msg.note]
-
-                wait_for_off[msg.note] = closeRow(
-                    note=msg.note,
-                    timestamp=(msg.time if msg.time > 0 else last_seen_time), # TODO : Should we just leave chords as they are?
-                    velocity=msg.velocity,
-                    note_length=0,
-                )
-                absolute_time[msg.note] = elapsed_time # time note started
-                last_seen_time = (msg.time if msg.time > 0 else last_seen_time) # in case there are two notes at the same time. But what about really short between?
-            elif msg.type == 'note_off':
-                if msg.note in wait_for_off: # To keep the timelength of each note we must assume it is the same note and take the cumulative time
-                    wait_row = wait_for_off.pop(msg.note)
-                    abs_time = absolute_time.pop(msg.note)
-                    row = closeRow(
-                        note=wait_row['note_int'],
-                        timestamp=wait_row['time'],
-                        velocity=wait_row['velocity'],
-                        note_length=(elapsed_time - abs_time),
-                    )
-                    notes.append(row)
-
-                    columns['note'].append(wait_row['note_int'])
-                    columns['timedelta'].append(wait_row['time'])
-                    columns['velocity'].append(wait_row['velocity'])
-                    columns['length'].append(wait_row['note_length'])
-                    # del wait_for_off[msg.note]
-
-            write_csv(notes, f, str(i))
+        write_csv(notes, f, f"{str(i)}_{instrument.name}")
 
         tracks.append(notes)
-
-        # write_csv(columns, f, str(i))
 
     # time, vel, pitch, prog = zip(*events)
     # torch.save(dict(
@@ -159,9 +96,6 @@ def processFile(f):
     # ), output_file(f, 'pkl'))
 
     return tracks
-
-
-
 
 
 if __name__ == "__main__":
@@ -176,11 +110,12 @@ if __name__ == "__main__":
 
         return files
 
-    files = get_files('/Users/karljohann/Downloads/archive/Jazz Midi/**') # all files and subfolders
+    files = get_files(INPUT_FILES_PATH + "/**") # all files and subfolders
     asd = 0
     for f in files:
         print(f)
-        pprint(processFile(f))
+        (processFile(f))
+        # pprint(processFile(f))
         if asd == 1:
             break
         asd +=1
